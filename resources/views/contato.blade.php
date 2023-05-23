8c46dd10715e55cbea67a5458752a89c279dce47
yuig100 <54219998+yuig100@users.noreply.github.com>
2023-05-23 18:13:51 -0300

    first commit

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+@section('title','Contato')
+@section('content')
+<div>
+    <h1>Contatos</h1>
+    <h3>
+        <a href="https://www.linkedin.com/in/pedro-henrique-massa-da-silva-gomes-6620571a4/" target="_blank">Linkedin</a>
+    </h3>
+    <h3>
+        <a href="https://github.com/yuig100" target="_blank" >GitHub</a>
+    </h3>
+</div>
+@endsection
